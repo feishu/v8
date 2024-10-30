3205895da12911d41aa446cc2831714c990cda06
Marja Hölttä <marja@chromium.org>
2024-10-30 11:42:59 +0100

    [maglev] Support faster string add for the StringOrStringWrapper type
    Bug: v8:12199
    Change-Id: If19741a1dc8c1cb2ee615f9b64adc6674d983646

--- src/maglev/maglev-graph-builder.cc
+++ src/maglev/maglev-graph-builder.cc
@@ -2540,7 +2540,19 @@ void MaglevGraphBuilder::VisitBinaryOperation() {
       }
       break;
     case BinaryOperationHint::kStringOrStringWrapper:
-    // TODO(v8:12199): Fast path for the string wrapper case.
+      if constexpr (kOperation == Operation::kAdd) {
+        if (broker()
+                ->dependencies()
+                ->DependOnStringWrapperToPrimitiveProtector()) {
+          ValueNode* left = LoadRegister(0);
+          ValueNode* right = GetAccumulator();
+          BuildCheckStringOrStringWrapper(left);
+          BuildCheckStringOrStringWrapper(right);
+          SetAccumulator(AddNewNode<StringWrapperConcat>({left, right}));
+          return;
+        }
+      }
+      [[fallthrough]];
     case BinaryOperationHint::kBigInt:
     case BinaryOperationHint::kBigInt64:
     case BinaryOperationHint::kAny:
@@ -4052,6 +4064,12 @@ void MaglevGraphBuilder::BuildCheckString(ValueNode* object) {
   NodeType known_type;
   if (EnsureType(object, NodeType::kString, &known_type)) return;
   AddNewNode<CheckString>({object}, GetCheckType(known_type));
+}
+
+void MaglevGraphBuilder::BuildCheckStringOrStringWrapper(ValueNode* object) {
+  NodeType known_type;
+  if (EnsureType(object, NodeType::kStringOrStringWrapper, &known_type)) return;
+  AddNewNode<CheckStringOrStringWrapper>({object}, GetCheckType(known_type));
 }
 
 void MaglevGraphBuilder::BuildCheckNumber(ValueNode* object) {
